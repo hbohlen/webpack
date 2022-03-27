ad980aaf7c27a226c9149216250a639b4051e236
hbohlen <bohlenhayden@gmail.com>
2022-03-26 22:01:34 -0500

    updated start script

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,5 @@
 import '../styles/index.scss';
 
-import Recipes from './Recipes';
-import sword from '../images/swc-sword.png';
-import swordSvg from '../images/sword.svg';
-
 export default function App() {
-  return (
-    <>
-      <section className="hero"></section>
-      <main>
-        <section>
-          <h1>oh Hai, React</h1>
-        </section>
-        <img src={sword} alt="sword" width="250" />
-        <img src={swordSvg} alt="sword" width="250" />
-        <Recipes />
-      </main>
-    </>
-  );
+  return <>App</>;
 }
